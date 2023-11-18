22bd7c197aa449cd7730e3847840c0764c628280
LuisFXZ <luisflores240903@gmail.com>
2023-11-18 16:57:39 -0500

    develop commmit

--- app/src/main/java/com/luisfxz/log_res_pru/RegisterActivity.kt
+++ app/src/main/java/com/luisfxz/log_res_pru/RegisterActivity.kt
@@ -5,18 +5,18 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
+class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_register)
 
-        val GoToRegister = findViewById<TextView>(R.id.go_to_register)
+        val GoToRegister = findViewById<TextView>(R.id.go_to_login)
         GoToRegister.setOnClickListener{goToRegister()}
 
     }
 
-    private fun goToRegister (){
-        val i = Intent(this,RegisterActivity::class.java)
+    private fun goToRegister(){
+        val i = Intent(this,MainActivity::class.java)
         startActivity(i)
     }
 
